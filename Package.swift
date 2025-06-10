846f3c0c0db02ba96d0fb7a52ac0cbcbe78a8591
Steven Lee <lisiwei_inbox@hotmail.com>
2025-06-10 22:47:13 +0800

    fix: ios compatible issue

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "PosxIminCashdrawer",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "PosxIminCashdrawer",
