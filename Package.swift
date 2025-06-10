7002164a775e3ab8fd7c84d108e725058ccc636e
Steven Lee <lisiwei_inbox@hotmail.com>
2025-06-10 22:55:41 +0800

    update: ios min target version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "PosxIminCashdrawer",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "PosxIminCashdrawer",
